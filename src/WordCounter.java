a75c70ca73470eb0844cb49a77d61f6ae78740a9
prabinthapa10 <prabinmagar636@gmail.com>
2025-06-27 15:12:49 +0545

    comment

--- src/WordCounter.java
+++ src/WordCounter.java
@@ -4,6 +4,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class WordCounter {
+    // comment
 
     public static void main(String[] args) {
         // Create the JFrame
